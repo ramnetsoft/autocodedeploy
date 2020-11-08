763d219f92873a5d4def246b4cedd363d70f59fe
ramnetsoft <ram@ramnetsoftware.com>
2020-11-08 18:02:12 -0500

    Create jiratosnowconfigQA.sh

---
+++ scripts/#!/bin/bash cd ~ #cd jiratosnow/jiratosnow #bash_profile . ~/.bash_profile sudo systemctl start docker echo $(pwd) cd jiratosnow/jiratosnow/jiratosnowconfigQA.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo $(pwd)
+cd ~
+echo $(pwd)
+git pull https://git-codecommit.us-east-1.amazonaws.com/v1/repos/JiraToSnowDev jiratosnowdevtoqa 
+#cd jiratosnow/jiratosnow
+#bash_profile
+#. ~/.bash_profile
+#sudo systemctl start docker
+#echo $(pwd)
+#cd jiratosnow/jiratosnow/jiratosnow
+#echo $(pwd)
+#make build_with_docker
